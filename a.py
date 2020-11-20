f8c48c92dd90a5f953b36646d0f65dd146d33f8d
Monica Bustamante <lilianalambda@gmail.com>
2020-11-20 15:22:13 -0800

    'update'

--- a.py
+++ a.py
@@ -8,6 +8,7 @@ import tensorflow
 import matplotlib
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import hist
 from keras.utils import to_categorical
 from tensorflow.keras.models import Sequential, Model 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -23,129 +24,248 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import streamlit as st
+import streamlit.components.v1 as components
 
 
+def main():
+    # Register pages
+    pages = {
+        "Home": Home,
+        "Graphs": Graph,
+        "Prediction_model": Prediction_model,
+        # 'Statement': Statement,
+        # "Stock": Stock,
+        # "Profit": Profit,
+    }
+    st.sidebar.title("Image Analysis")
+    page = st.sidebar.selectbox("Select Menu", tuple(pages.keys()))
+    pages[page]()
 
-# Load data with path
-data_set = Path('chest_xray/chest_xray')
-train_1 = data_set / 'train'
-val_1 = data_set / 'val'
-test_1 = data_set / 'test'
-normal_lung = train_1 / 'NORMAL'
-pneumonia_lung = train_1 / 'PNEUMONIA'
+def Home():
+    def main():
+        st.markdown("<h1 style='text-align: center; color: #002966;'>Image Classification</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; color: #002966;'>Pneumonia</h1>", unsafe_allow_html=True)
+        st.write(
+        """
+        Artificial Intelligence  ! 
+        
+        :
+        -   
+        -  
+        -  
+        -  
+        -  
+        ---
+        """)
+        page_bg_img = '''
+            <style>
+            body {
+            background-image: url("https://images.pexels.com/photos/1024613/pexels-photo-1024613.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1000");
+            background-size: cover;
+            }
+            </style>
+            '''
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+        footer_temp1 = """
+            <!-- CSS  -->
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+            <link href="static/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+            <footer class="background-color: blue">
+                <div class="container" id="About App">
+                <div class="row">
+                    <div class="col l6 s12">
+                    </div>
+            <div class="col l3 s12">
+                    <h5 class="black-text">Connect With Me</h5>
+                    <ul>
+                        <a href="http://www.monicadatascience.com/" target="#002966" class="black-text">
+                        ❤<i class="❤"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/monica-bustamante-b2ba3781/3" target="#002966" class="black-text">
+                        <i class="fab fa-linkedin fa-4x"></i>
+                    <a href="http://www.monicadatascience.com/" target="#002966" class="black-text">
+                    ❤<i class="❤"></i>
+                    </a>
+                    <a href="https://github.com/Moly-malibu/Image Analysis" target="#002966" class="black-text">
+                        <i class="fab fa-github-square fa-4x"></i>
+                    <a href="http://www.monicadatascience.com/" target="#002966" class="black-text">
+                    ❤<i class="❤"></i>
+                    </a>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+                <div class="footer-copyright">
+                <div class="container">
+                Made by <a class="black-text text-lighten-3" href="http://www.monicadatascience.com/">Monica Bustamante</a><br/>
+                <a class="black-text text-lighten-3" href=""> @Copyrigh</a>
+                </div>
+                </div>
+            </footer>
+            """
+        components.html(footer_temp1,height=500)
 
-# Get all the images
-normal_0 = normal_lung.glob('*.jpeg')
-pneumonia_1 = pneumonia_lung.glob('*.jpeg')
+    if __name__ == "__main__":
+        main()
 
-train_data = [] #insert the data into this list
+title_temp = """
+	 <!-- CSS  -->
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	  <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	  <link href="static/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	 <footer class="background-color: blue">
+	    <div class="container" id="About App">
+	      <div class="row">
+	        <div class="col l6 s12">
+                <h5 class="black-text">Artificial Intelligence</h5>
+	          <p class="grey-text text-lighten-4">Using Streamlit, Seaborn, Sklearn, Tensorflow,  Keras.</p>
+	        </div>     
+	  </footer>
+	"""
+components.html(title_temp,height=100)
+    
 
-for img in normal_0: #normal cases = 0. 
-    train_data.append((img,0))
-for img in pneumonia_1: # pneumonia cases = 1. 
-    train_data.append((img, 1))
-train_data = pd.DataFrame(train_data, columns=['image', 'label'],index=None) #convert with pandas dataframe from the data list
-train_data = train_data.sample(frac=1.).reset_index(drop=True) # sample and reset index data 
-(train_data.info())
+def Graph():
+    # Load data with path
+    data_set = Path('chest_xray/chest_xray')
+    train_1 = data_set / 'train'
+    val_1 = data_set / 'val'
+    test_1 = data_set / 'test'
+    normal_lung = train_1 / 'NORMAL'
+    pneumonia_lung = train_1 / 'PNEUMONIA'
 
-# #Plot 
-cases_count = train_data['label'].value_counts()
-plt.figure(figsize=(10,8))
-ax = sns.barplot(x=cases_count, y= cases_count.values, ci=68)
-plt.title("X-Ray records of the lungs")
-color_patch = mpatches.Patch(color ='#EDB120', label="Pneumonia")
-plt.legend(handles=[color_patch])
-plt.show()
+    # Get all the images
+    normal_0 = normal_lung.glob('*.jpeg')
+    pneumonia_1 = pneumonia_lung.glob('*.jpeg')
 
-# #Model
-# batch_size = 16
-# epochs = 50
-# img_height = 224
-# img_width = 224
-# train_sample = 5217
-# val_sample = 17
+    train_data = [] #insert the data into this list
 
-# train_data = ImageDataGenerator(rescale=1./255)
-# test_data = ImageDataGenerator(rescale=1./255)
+    for img in normal_0: #normal cases = 0. 
+        train_data.append((img,0))
+    for img in pneumonia_1: # pneumonia cases = 1. 
+        train_data.append((img, 1))
+    train_data = pd.DataFrame(train_data, columns=['image', 'label'],index=None) #convert with pandas dataframe from the data list
+    train_data = train_data.sample(frac=1.).reset_index(drop=True) # sample and reset index data 
+    (train_data.info())
 
-# #Model
-# resnet = ResNet50(weights='imagenet', include_top=False )
+    # #Plot 
+    cases_count = train_data['label'].value_counts()
+    plt.figure(figsize=(10,8))
+    ax = sns.barplot(x=cases_count, y= cases_count.values, ci=68)
+    plt.title("X-Ray records of the lungs")
+    color_patch = mpatches.Patch(color ='#EDB120', label="Pneumonia")
+    plt.legend(handles=[color_patch])
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
+    st.write('***Statistical Data Analysis***')
+    st.table(train_data.describe())
 
-# for layer in resnet.layers:
-#     layer.trainable = False
+    #Graph train data
+    # hist = (train_data.label).transpose() 
+    # st.bar_chart(hist)
 
-# x = resnet.output
-# x = GlobalAveragePooling2D()(x) #layer flatten
-# x = Dense(1024, activation='relu')(x)
-# predictions = Dense(1, activation='sigmoid')(x)
-# model = Model(resnet.input, predictions)
+def Prediction_model():
+    data_set = Path('chest_xray/chest_xray')
+    train_1 = data_set / 'train'
+    val_1 = data_set / 'val'
+    test_1 = data_set / 'test'
+    normal_lung = train_1 / 'NORMAL'
+    pneumonia_lung = train_1 / 'PNEUMONIA'
 
-# # Setup Architecture
+    #Model
+    batch_size = 16
+    epochs = 50
+    img_height = 224
+    img_width = 224
+    train_sample = 5217
+    val_sample = 17
 
-# custom_cnn = Sequential([
-#     Conv2D(64, (3,3), input_shape=(224, 224, 3), padding='same', activation='relu'),
-#     MaxPooling2D((2,2), padding='same'),
-#     Conv2D(32, (3,3), activation='relu'),
-#     MaxPooling2D((2,2)),
-#     Conv2D(16, (3,3), activation='relu'),
-#     MaxPooling2D((2,2)),
-#     Conv2D(8,(2,2), activation='relu'),
-#     MaxPooling2D((2,2)),
-#     Flatten(),
-#     Dense(64, activation='relu'),
-#     Dense(1, activation='sigmoid')
-# ])
-# custom_cnn.summary()
+    train_data = ImageDataGenerator(rescale=1./255)
+    test_data = ImageDataGenerator(rescale=1./255)
 
-# #compile Model
-# model.compile(optimizer='rmsprop', 
-#               loss='binary_crossentropy', 
-#               metrics=['accuracy'])
+    #Model
+    resnet = ResNet50(weights='imagenet', include_top=False )
 
-# train_data = ImageDataGenerator(
-#     rescale=1. / 255,
-#     shear_range=0.2,
-#     zoom_range=0.2,
-#     horizontal_flip=True)
+    for layer in resnet.layers:
+        layer.trainable = False
 
-# train_generator = train_data.flow_from_directory(
-#     train_1,
-#     target_size=(img_width, img_height),
-#     batch_size=batch_size,
-#     class_mode='binary')
+    x = resnet.output
+    x = GlobalAveragePooling2D()(x) #layer flatten
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(1, activation='sigmoid')(x)
+    model = Model(resnet.input, predictions)
 
-# validation_generator = test_data.flow_from_directory(
-#     val_1,
-#     target_size=(img_width, img_height),
-#     batch_size=batch_size,
-#     class_mode='binary')
+    # # Setup Architecture
 
-# test_generator = test_data.flow_from_directory(
-#     test_1,
-#     target_size=(img_width, img_height),
-#     batch_size=batch_size,
-#     class_mode='binary')
+    custom_cnn = Sequential([
+        Conv2D(64, (3,3), input_shape=(224, 224, 3), padding='same', activation='relu'),
+        MaxPooling2D((2,2), padding='same'),
+        Conv2D(32, (3,3), activation='relu'),
+        MaxPooling2D((2,2)),
+        Conv2D(16, (3,3), activation='relu'),
+        MaxPooling2D((2,2)),
+        Conv2D(8,(2,2), activation='relu'),
+        MaxPooling2D((2,2)),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+    custom_cnn.summary()
 
-# history = model.fit(
-#     train_generator,
-#     steps_per_epoch=train_sample // batch_size,
-#     epochs=epochs,
-#     validation_data=validation_generator,
-#     validation_steps=val_sample// batch_size)
+    #compile Model
+    model.compile(optimizer='rmsprop', 
+                loss='binary_crossentropy', 
+                metrics=['accuracy'])
 
-# # evaluate the model
-# scores = Model.evaluate(test_generator)
-# print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    train_data = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-# sns.lineplot(data=pd.DataFrame(history.history));
+    train_generator = train_data.flow_from_directory(
+        train_1,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary')
 
-# #Analysis the Accuracy
-# plt.plot(history.history['accuracy'], label='accuracy')
-# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.ylim([0.5, 1])
-# plt.legend(loc='lower right')
+    validation_generator = test_data.flow_from_directory(
+        val_1,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    test_generator = test_data.flow_from_directory(
+        test_1,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    history = model.fit(
+        train_generator,
+        steps_per_epoch=train_sample // batch_size,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=val_sample// batch_size)
+
+    # evaluate the model
+    scores = Model.evaluate(test_generator)
+    st.write("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+    sns.lineplot(data=pd.DataFrame(history.history));
+
+    #Analysis the Accuracy
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
 
 # test_loss, test_acc = model.evaluate(test_generator, verbose=2)
 
@@ -265,3 +385,5 @@ plt.show()
 # df = pd.DataFrame(data, columns=['Class', 'Count'])
 
 # sns.barplot(x=df['Class'], y=df['Count']);
+if __name__=='__main__':
+    main()
